7dce9c66ec9d9fdc6bb573b39cf2e5494a0a9bea
Sai Hlaing Lu <saisailuhlaing@gmail.com>
2019-10-02 13:53:14 +0630

    Updates

---
+++ precache-manifest.67384b5c1e4c55c9597b71a1417a1ac3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c597495ac57ca98256a1a1b71b100222",
+    "url": "/react-basic-monsters.git/index.html"
+  },
+  {
+    "revision": "09ac4ddbf01a27157195",
+    "url": "/react-basic-monsters.git/static/css/main.3f6c3648.chunk.css"
+  },
+  {
+    "revision": "5888f0a1ac5fc31c0947",
+    "url": "/react-basic-monsters.git/static/js/2.bd8eeb61.chunk.js"
+  },
+  {
+    "revision": "09ac4ddbf01a27157195",
+    "url": "/react-basic-monsters.git/static/js/main.cb6331e7.chunk.js"
+  },
+  {
+    "revision": "a86f74d062200c3abea8",
+    "url": "/react-basic-monsters.git/static/js/runtime-main.0639c92b.js"
+  }
+]);
